965a00c87b816380c18a30272caeb4949aa1fbfc
Dipen Shrestha <dipenstha00@gmail.com>
2024-11-02 12:49:29 +0545

    fix: unwanted files deleted and content changes

--- data/index.ts
+++ data/index.ts
@@ -173,22 +173,22 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Python Developer Intern",
+      desc: "Assisted in the development of a web-based platform using Django, enhancing security.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: "AI Data Trainer",
+      desc: "Trained AI models to better interact with users.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      title: "Python Developer",
+      desc: "Contributed to the maintainence of Merojob - Online Job Portal.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
